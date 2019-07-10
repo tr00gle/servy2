c9d286970dfa2039fd38d2fbead4439427b7b65b
Ryan Trontz <rtrontz@gmail.com>
2019-07-10 14:34:12 -0700

    first local commit

---
+++ lib/servy2.ex
@@ -0,0 +1,18 @@
+defmodule Servy2 do
+  @moduledoc """
+  Documentation for Servy2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Servy2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/servy2_test.exs
@@ -0,0 +1,8 @@
+defmodule Servy2Test do
+  use ExUnit.Case
+  doctest Servy2
+
+  test "greets the world" do
+    assert Servy2.hello() == :world
+  end
+end
