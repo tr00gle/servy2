217330c1d63a39e5da9bcc74c10de063eed7e6e0
Ryan Trontz <rtrontz@gmail.com>
2019-07-11 10:54:44 -0700

    unit4: parsing the request with pattern matching and pipes

--- lib/servy2/handler.ex
+++ lib/servy2/handler.ex
@@ -8,7 +8,12 @@ defmodule Servy2.Handler do
 
   def parse(request) do
     # TODO: Parse the request string into a map:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+    [ method, path, _protocol ] = 
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+    %{ method: method, path: path resp_body: "" }
   end
 
   def route(conv) do
