a57fbe6260cf33797a0576c4d99ed4e74a8bdb6a
Ryan Trontz <rtrontz@gmail.com>
2019-07-13 17:03:21 -0700

    delegating delete to controller

--- lib/servy2/handler.ex
+++ lib/servy2/handler.ex
@@ -6,6 +6,7 @@ defmodule Servy2.Handler do
   """
 
   alias Servy2.Conv
+  alias Servy2.BearController
   # @pages_path Path.expand("../../pages", __DIR__)
   # Use this method when compiling with `iex -S mix`
   @pages_path Path.expand("pages", File.cwd!)
@@ -44,19 +45,20 @@ defmodule Servy2.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/bears" } = conv) do
-    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+    BearController.index(conv)
   end
 
   def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
-    %{ conv | status: 200, resp_body: "Bear #{id}" }
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
 
   def route(%Conv{ method: "DELETE", path: "/bears/" <> _id } = conv) do
-    %{ conv | status: 403, resp_body: "Sorry, we can't do deletions here."}
+    BearController.delete(conv, conv.params)
   end
 
   def route(%Conv{method: "POST", path: "/bears"} = conv) do
-    %{ conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear!" }
+    BearController.create(conv, conv.params)
   end
 
   def route(%Conv{ method: _method, path: path } = conv) do 
