114eae1dbe3164f2943f82511a10fc0ca0b4d684
Ryan Trontz <rtrontz@gmail.com>
2019-07-15 11:48:49 -0700

    doing some comprehensions exercises

---
+++ lib/servy2/bear_view.ex
@@ -0,0 +1,9 @@
+defmodule Servy2.BearView do
+  require EEx
+
+  @templates_path Path.expand("../../templates", __DIR__)
+
+  EEx.function_from_file :def, :index, Path.join(@templates_path, "index.eex"), [:bears]
+
+  EEx.function_from_file :def, :show, Path.join(@templates_path, "show.eex"), [:bear]
+end
