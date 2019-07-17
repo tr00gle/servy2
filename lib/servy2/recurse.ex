9d2dc7c1d5095de7f77d67ae6a7f18bf14be4832
Ryan Trontz <rtrontz@gmail.com>
2019-07-17 15:58:36 -0700

    removing 'return' keyword

--- lib/servy2/recurse.ex
+++ lib/servy2/recurse.ex
@@ -14,8 +14,7 @@ defmodule Servy2.Recurse do
 
   # better 
   def my_map([head | tail], fun) do
-    return 
-  [fun.(head) | my_map(tail, fun)]
+    [fun.(head) | my_map(tail, fun)]
   end
 
   def my_map([], _fun), do: []
