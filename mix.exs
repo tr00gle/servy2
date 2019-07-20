0f920be55caf230a185dd20e97b94aa79ae9f86a
Ryan Trontz <rtrontz@gmail.com>
2019-07-20 16:47:03 -0700

    dialyzer updating

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Servy2.MixProject do
     [
       app: :servy2,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.8.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
@@ -16,7 +16,8 @@ defmodule Servy2.MixProject do
         "coveralls.html": :test
       ],
       dialyzer: [
-        plt_add_apps: [:mix, :ex_unit]
+        plt_add_apps: [:mix, :ex_unit],
+        remove_defaults: [:unknown]
       ]
     ]
   end
@@ -35,7 +36,7 @@ defmodule Servy2.MixProject do
       {:poison, "~> 4.0"},
       {:earmark, "~> 1.3"},
       {:excoveralls, "~> 0.10", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false}
     ]
   end
