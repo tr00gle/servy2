f5383adf176a457758197b181c06d2c96c2b5d51
Ryan Trontz <rtrontz@gmail.com>
2019-08-11 16:53:01 -0700

    creating videocam module with get snapshot method

---
+++ lib/servy2/api/video_cam.ex
@@ -0,0 +1,14 @@
+defmodule Servy2.VideoCam do
+  @doc """
+    Simulates sending a request to an external API 
+    to get a snapshot image from a video camera
+  """
+  def get_snapshot(camera_name) do
+    
+    # sleep 2 seconds to simulate that the API can be slow
+    :timer.seep(2000)
+
+    # example response returned from the API:
+    "#{camera_name}-snapshot.jpg"
+  end
+end
