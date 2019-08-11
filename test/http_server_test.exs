98ce116036fce164ba536c85afcea3d199c53991
Ryan Trontz <rtrontz@gmail.com>
2019-08-11 15:32:00 -0700

    adding http server test by spawning a new process

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -4,4 +4,30 @@ defmodule HTTPServerTest do
   More tests here, please.
   Even more tests, please.
   """
+  use ExUnit.Case
+
+  alias Servy.HttpServer
+  alias Servy.HttpClient
+
+  test "accepts a request on a socket and sends back a response" do
+    spawn(HttpServer, :start, [4000])
+
+    request = """
+    GET /wildthings HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = HttpClient.send_request(request)
+
+    assert response == """
+           HTTP/1.1 200 OK\r
+           Content-Type: text/html\r
+           Content-Length: 20\r
+           \r
+           Bears, Lions, Tigers
+           """
+  end
 end
