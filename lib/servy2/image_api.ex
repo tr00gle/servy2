5e9421580537ec673323588d19fe7ff7591119ac
Ryan Trontz <rtrontz@gmail.com>
2019-08-12 10:19:43 -0700

    running mix format

--- lib/servy2/image_api.ex
+++ lib/servy2/image_api.ex
@@ -1,7 +1,7 @@
 defmodule Servy2.ImageAPI do
   def query(image_id) do
     api_url(image_id)
-    |> HTTPoison.get
+    |> HTTPoison.get()
     |> handle_response
   end
 
@@ -10,15 +10,16 @@ defmodule Servy2.ImageAPI do
   end
 
   defp handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
-    image_url = 
+    image_url =
       body
       |> Poison.Parser.parse!(%{})
       |> get_in(["image", "image_url"])
+
     {:ok, image_url}
   end
 
   defp handle_response({:ok, %HTTPoison.Response{status_code: _status_code, body: body}}) do
-    message = 
+    message =
       body
       |> Poison.Parser.parse!(%{})
       |> get_in(["message"])
--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -10,30 +10,29 @@ defmodule HTTPServerTest do
 
   test "accepts a request on a socket and sends back a response" do
     spawn(HTTPServer, :start, [4000])
-  
+
     parent = self()
-  
+
     max_concurrent_requests = 5
-  
+
     # Spawn the client processes
     for _ <- 1..max_concurrent_requests do
       spawn(fn ->
         # Send the request
-        {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
-  
+        {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+
         # Send the response back to the parent
         send(parent, {:ok, response})
       end)
     end
-  
+
     # Await all {:handled, response} messages from spawned processes.
     for _ <- 1..max_concurrent_requests do
-      receive do 
-        {:ok, response} -> 
+      receive do
+        {:ok, response} ->
           assert response.status_code == 200
           assert response.body == "Bears, Lions, Tigers"
       end
     end
   end
-  
 end
