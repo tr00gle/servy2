616f6329683571bd11282929c4a7c7b60517d8eb
Ryan Trontz <rtrontz@gmail.com>
2019-08-12 11:08:58 -0700

    making tracker module to find bigfoot

--- lib/servy2/fetcher.ex
+++ lib/servy2/fetcher.ex
@@ -2,7 +2,7 @@ defmodule Servy2.Fetcher do
   def async(camera_name) do
     caller = self()
 
-    spawn(fn -> send(parent, {:result, self(), Servy2.VideoCam.get_snapshot()}) end)
+    spawn(fn -> send(caller, {:result, self(), Servy2.VideoCam.get_snapshot(camera_name)}) end)
   end
 
   def get_result(pid) do
