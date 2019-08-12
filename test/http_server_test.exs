d64b27567ce5eb42167706da6dc33be5e22a5e4e
Ryan Trontz <rtrontz@gmail.com>
2019-08-12 10:19:19 -0700

    updating server test

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -1,33 +1,39 @@
-# defmodule HTTPServerTest do
-#   @moduledoc """
-#   Tests for the HTTP Server module.
-#   More tests here, please.
-#   Even more tests, please.
-#   """
-#   use ExUnit.Case
+defmodule HTTPServerTest do
+  @moduledoc """
+  Tests for the HTTP Server module.
+  More tests here, please.
+  Even more tests, please.
+  """
+  use ExUnit.Case
 
-#   alias Servy2.HTTPServer
-#   alias Servy2.HTTPClient
+  alias Servy2.HTTPServer
 
-#   test "accepts a request on a socket and sends back a response" do
-#     spawn(HTTPServer, :start, [4000])
-
-#     request = """
-#     GET /wildthings HTTP/1.1\r
-#     Host: example.com\r
-#     User-Agent: ExampleBrowser/1.0\r
-#     Accept: */*\r
-#     \r
-#     """
-
-#     response = HTTPClient.send_request(request)
-
-#     assert response == """
-#            HTTP/1.1 200 OK\r
-#            Content-Type: text/html\r
-#            Content-Length: 20\r
-#            \r
-#            Bears, Lions, Tigers
-#            """
-#   end
-# end
+  test "accepts a request on a socket and sends back a response" do
+    spawn(HTTPServer, :start, [4000])
+  
+    parent = self()
+  
+    max_concurrent_requests = 5
+  
+    # Spawn the client processes
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        # Send the request
+        {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+  
+        # Send the response back to the parent
+        send(parent, {:ok, response})
+      end)
+    end
+  
+    # Await all {:handled, response} messages from spawned processes.
+    for _ <- 1..max_concurrent_requests do
+      receive do 
+        {:ok, response} -> 
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
+  end
+  
+end
