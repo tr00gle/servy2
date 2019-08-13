f24df7f2e59ccc089bf63c14e7b08fec30462c3e
Ryan Trontz <rtrontz@gmail.com>
2019-08-12 22:00:03 -0700

    changing process name atom to module attribute to make future changes easier

--- lib/servy2/pledge_server.ex
+++ lib/servy2/pledge_server.ex
@@ -1,8 +1,10 @@
 defmodule Servy2.PledgeServer do
+  @name :pledge_server
+
   def start do
     IO.puts("starting the pledge server...")
     pid = spawn(__MODULE__, :listen_loop, [[]])
-    Process.register(pid, :pledge_server)
+    Process.register(pid, @name)
     pid
   end
 
@@ -12,7 +14,6 @@ defmodule Servy2.PledgeServer do
         {:ok, id} = send_pledges_to_service(name, amount)
         most_recent = Enum.take(state, 2)
         new_state = [{name, amount} | most_recent]
-        List.pop_at(new_state, -1)
         listen_loop(new_state)
         send(sender, {:response, id})
 
@@ -22,16 +23,16 @@ defmodule Servy2.PledgeServer do
     end
   end
 
-  def create_pledge(pid, name, amount) do
-    send(pid, {self(), :create_pledge, name, amount})
+  def create_pledge(name, amount) do
+    send(@name, {self(), :create_pledge, name, amount})
 
     receive do
       {:response, status} -> status
     end
   end
 
-  def recent_pledges(pid) do
-    send(pid, {self(), :recent_pledges})
+  def recent_pledges do
+    send(@name, {self(), :recent_pledges})
 
     receive do
       {:response, pledges} -> pledges
