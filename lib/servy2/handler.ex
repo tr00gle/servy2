15fa3a910569d4e63b315dac80cdccf8a3ba5df5
Ryan Trontz <rtrontz@gmail.com>
2019-08-12 19:57:00 -0700

    switching to Task module and adding timeout limits

--- lib/servy2/handler.ex
+++ lib/servy2/handler.ex
@@ -46,7 +46,7 @@ defmodule Servy2.Handler do
     snapshots =
       ["cam-1", "cam-2", "cam-3"]
       |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
-      |> Enum.map(&Task.await/1)
+      |> Enum.map(&Task.await(&1, 6000))
 
     where_is_bigfoot = Task.await(task)
 
