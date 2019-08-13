e1780549b9578e869f21437e304c3ddbafa8f6c9
Ryan Trontz <rtrontz@gmail.com>
2019-08-12 23:02:56 -0700

    creating pledge controller

---
+++ lib/servy2/pledge_controller.ex
@@ -0,0 +1,14 @@
+defmodule Servy2.PledgeController do
+  
+  def create(conv, %{"naem" => name, "amount" => amount}) do
+    Servy2.PledgeServer.create_pledge(name, String.to_integer(amount))
+
+    %{conv | status: 201, resp_body: "#{name} pledged #{amount}"}
+  end
+
+  def index(conv) do
+    pledges = Servy2.PledgeServer.recent_pledges()
+
+    %{ conv | status: 200, resp_body: inspect(pledges)}
+  end
+end
